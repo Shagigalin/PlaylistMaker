e040d6f2b66bab65227a748231fe416f003bf772
Gazinur <ufa-gazinur@mail.ru>
2025-07-17 10:49:27 +0500

    v3 создал 3 рабочие кнопки

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textview.MaterialTextView
 
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var buttonBack: Button
+    private lateinit var buttonBack: MaterialTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
