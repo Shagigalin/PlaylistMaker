bec092c72a14ce5b18b6ec996b88206370aff9b5
Gazinur <ufa-gazinur@mail.ru>
2025-07-26 10:58:31 +0500

    реализована темная тема без кнопки

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,26 +1,74 @@
 package com.example.playlistmaker
 
-import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textview.MaterialTextView
-
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var buttonBack: MaterialTextView
+    private lateinit var temaButton: TextView
+    private val PREFS_NAME = "theme_prefs"
+    private val THEME_KEY = "current_theme"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+        val isDarkTheme = prefs.getBoolean(THEME_KEY, false)
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        buttonBack = findViewById(R.id.button_back)
-        buttonBack.setOnClickListener {
-            finish()
+        findViewById<TextView>(R.id.button_back).setOnClickListener {
+            onBackPressed()
         }
 
+
+        temaButton = findViewById(R.id.tema_button)
+
+
+        updateButtonUI(isDarkTheme)
+
+
+        temaButton.setOnClickListener {
+            toggleTheme()
+        }
     }
 
+    private fun toggleTheme() {
+        val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+        val isDarkTheme = prefs.getBoolean(THEME_KEY, false)
 
+        if (isDarkTheme) {
+
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            prefs.edit().putBoolean(THEME_KEY, false).apply()
+            updateButtonUI(false)
+        } else {
+
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            prefs.edit().putBoolean(THEME_KEY, true).apply()
+            updateButtonUI(true)
+        }
+
+
+        recreate()
+    }
+
+    private fun updateButtonUI(isDarkModeEnabled: Boolean) {
+        if (isDarkModeEnabled) {
+            temaButton.text = getString(R.string.tema_dark)
+            temaButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.on, 0)
+        } else {
+            temaButton.text = getString(R.string.tema_light)
+            temaButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.off, 0)
+        }
+    }
 }
